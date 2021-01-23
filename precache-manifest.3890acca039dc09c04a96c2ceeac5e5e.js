54729cd35619abb15174aabee6836beb10a76f1c
mosogrean1 <mosogrean@gmail.com>
2021-01-23 11:14:31 +0700

    Updates

---
+++ precache-manifest.3890acca039dc09c04a96c2ceeac5e5e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3206f9dfdb14ce617b3756f5b94143dc",
+    "url": "/tradingboard/index.html"
+  },
+  {
+    "revision": "74492cc645d02a959d44",
+    "url": "/tradingboard/static/css/main.35aa1781.chunk.css"
+  },
+  {
+    "revision": "d46a7d33061dc9a16d8a",
+    "url": "/tradingboard/static/js/2.35d7dd32.chunk.js"
+  },
+  {
+    "revision": "a34b9ff819ddad9a3b32ca5e38be39e3",
+    "url": "/tradingboard/static/js/2.35d7dd32.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "74492cc645d02a959d44",
+    "url": "/tradingboard/static/js/main.97154029.chunk.js"
+  },
+  {
+    "revision": "a10b9cb5493c9d0f0d41",
+    "url": "/tradingboard/static/js/runtime-main.a22fb6f6.js"
+  }
+]);
