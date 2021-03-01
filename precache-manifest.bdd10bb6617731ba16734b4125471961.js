65f7943c54764ae1266cd6ef980ade82e4f05e07
mosogrean1 <mosogrean@gmail.com>
2021-03-01 23:51:06 +0700

    Updates

---
+++ precache-manifest.bdd10bb6617731ba16734b4125471961.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "027648940b07e0c0a2d07b23362592e0",
+    "url": "/tradingboard/index.html"
+  },
+  {
+    "revision": "7dcce2f2801b2132dcc4",
+    "url": "/tradingboard/static/css/main.5e3e0f55.chunk.css"
+  },
+  {
+    "revision": "8a538820f0509334a135",
+    "url": "/tradingboard/static/js/2.20a469dc.chunk.js"
+  },
+  {
+    "revision": "d81e8c133b990ccb4be3c4ba7e9b86dc",
+    "url": "/tradingboard/static/js/2.20a469dc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7dcce2f2801b2132dcc4",
+    "url": "/tradingboard/static/js/main.e2eddc67.chunk.js"
+  },
+  {
+    "revision": "a10b9cb5493c9d0f0d41",
+    "url": "/tradingboard/static/js/runtime-main.a22fb6f6.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/tradingboard/precache-manifest.918d240cbedbc198d1dd41c690dc7685.js"
+  "/tradingboard/precache-manifest.bdd10bb6617731ba16734b4125471961.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -35,5 +35,5 @@ workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
 workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/tradingboard/index.html"), {
   
-  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
